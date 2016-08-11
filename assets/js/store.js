0afebb340917d9e6c5bc814185411eba8bb58b87
Matt Ayre <mattayre@Matts-MacBook-Pro.local>
2016-08-11 21:34:29 +0100

    17

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,10 +1,8 @@
  //Authenticate moltin and then call addProductMain to add product and avoid callback
 var moltin = new Moltin({publicId: 'vn6jZewrOYsgm2fkKoDCrHbsg7GTsz9YuvG9kyP8PG'}); 
-//alert('reload');
+
 function addProduct(productname, productnumber){
-  //alert("Start of addProduct");
   moltin.Authenticate(function() {
-    //alert("Authentication complete");
     addProductMain(productname, productnumber);
   });
 }
@@ -17,50 +15,32 @@ function addProductMain(productname, productnumber){
   alert("addProductMain Number of items in cart now "+cart.total_items);
 }
 function createCart(){
-  //alert("Start of createCart");
   moltin.Authenticate(function() {
-    //alert("Authentication complete");
     createCartMain();
   });
 }
 function createCartMain(){
-//alert("createCartMain Number of items in cart now "+cart.total_items);
-  alert("Start of createCartMain");
-  
-  //Insert a product in cart
- /* var product = moltin.Product.Find({slug: 'epiphone-335-ebony'});
-  var item = moltin.Cart.Insert(product[0].id, 4, null);
-  alert("Product "+product[0].id+" added to cart");
-  var product = moltin.Product.Find({slug: 'epiphone-335-cherry'});
-  var item = moltin.Cart.Insert(product[0].id, 6, null);
-  alert("Product "+product[0].id+" added to cart");*/
-
   // create var cart which contains cart contents
   var cart = moltin.Cart.Contents();
-  alert("Number of items in cart now "+cart.total_items);
-  
-
   //loop through cart collection and each time load html and moltin contents 
   //to div id 'creatCart' with that loops product details
   var line = '';
-  console.log(cart.contents);
   for (var product in cart.contents) {
-        line += '<tr>';
-        line += '<td>' + '<div class="img-container">';
-        line += '<img src="assets/img/' + cart.contents[product].slug + '.jpg' + '" alt="..."/>'; 
-        line += '</div>' + '</td>';
-        line += '<td class = "td-name">';
-        line += '<h4 id = "productName">' + cart.contents[product].title +'</td>';
-        line += '<td>';
-        line += '<p id = "description">' + cart.contents[product].description +'</p>';
-        line += '<td class = "td-number">';
-        line += '<h4 id = "price">' + cart.contents[product].pricing.formatted.without_tax+'</td>';
-        line += '<td class = "td-number">';
-        line += '<h4 id = "quantity">' + cart.contents[product].quantity+'</td>';
-        line += '<td class = "td-number">';
-        line += '<h4 id = "total">' + cart.contents[product].totals.pre_discount.formatted.without_tax +'</td></tr>';
+      line += '<tr>';
+      line += '<td>' + '<div class="img-container">';
+      line += '<img src="assets/img/' + cart.contents[product].slug + '.jpg' + '" alt="..."/>'; 
+      line += '</div>' + '</td>';
+      line += '<td class = "td-name">';
+      line += '<h4 id = "productName">' + cart.contents[product].title +'</td>';
+      line += '<td>';
+      line += '<p id = "description">' + cart.contents[product].description +'</p>';
+      line += '<td class = "td-number">';
+      line += '<h4 id = "price">' + cart.contents[product].pricing.formatted.without_tax+'</td>';
+      line += '<td class = "td-number">';
+      line += '<h4 id = "quantity">' + cart.contents[product].quantity+'</td>';
+      line += '<td class = "td-number">';
+      line += '<h4 id = "total">' + cart.contents[product].totals.pre_discount.formatted.without_tax +'</td></tr>';
   } 
-  
   // load total of cart and html to id 'createCart' and display in page
   line += '<tr>'; 
   line += '<td colspan="2"></td>';
@@ -72,7 +52,6 @@ function createCartMain(){
   line += '<td></td>';
   line += '</tr>';
   el = document.getElementById('createCart');
-  alert(line);
   el.innerHTML = line; 
 }
 
